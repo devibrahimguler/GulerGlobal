35977574665a3c073b0cf2b72ab92f4d202146b6
ibrahim <04190000633@ogrenci.ege.edu.tr>
2024-01-20 15:29:28 +0300

    first commit

---
+++ GulerGlobal/View/Helperü/Cards/Card.swift
@@ -0,0 +1,18 @@
+//
+//  Card.swift
+//  GulerGlobal
+//
+//  Created by ibrahim Güler on 20.01.2024.
+//
+
+import SwiftUI
+
+struct Card: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Card()
+}
---
+++ GulerGlobal/View/Helperü/Cards/WorkCard.swift
@@ -0,0 +1,18 @@
+//
+//  WorkCard.swift
+//  GulerGlobal
+//
+//  Created by ibrahim Güler on 20.01.2024.
+//
+
+import SwiftUI
+
+struct WorkCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkCard()
+}
